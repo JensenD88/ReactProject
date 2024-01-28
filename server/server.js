83cee7e956802639397a151294607a8efb1998c6
Darren <Darrenjensen88@gmail.com>
2024-01-28 22:33:07 +0530

    third commit after GET APi in Admin page

--- server/server.js
+++ server/server.js
@@ -29,9 +29,19 @@ mongoose.connect("mongodb://localhost:27017/UserList");
  	Password: String,
  })
 
+ const bookdata = new mongoose.Schema({
+	name: String,
+	author: String,
+	price: Number,
+	quantity: Number,
+
+ })
+
 userList.plugin(passportLocalMongoose);
 
 const User = new mongoose.model("users", userList);
+const book = new mongoose.model("bookData", bookdata);
+
 
 passport.use(User.createStrategy());
 passport.serializeUser(User.serializeUser());
@@ -41,33 +51,38 @@ app.get("/api",(req,res)=>{
   res.status(200).send('server working on port 3500');
 })
 
-app.post("/api/login", (req,res)=>{
-
-  const user = new User({
+app.post("/api/login", (req, res) => {
+	const user = new User({
 		username: req.body.username,
 		password: req.body.password
 	})
-	
-	const Data = JSON.stringify(user.username);
-
-	req.login(user, function(err){
-		if(err){
-			console.log("login error");
+	//const Data = JSON.stringify(user.username);
+	req.login(user, function (err) {
+		if (err) {
+			console.log("login error", err);
 		} else {
-			
-			passport.authenticate("local", { failureRedirect: '/login' })(req, res, function(){
-				User.findOne({username : user.username}).then((doc)=>{
+			passport.authenticate("local", { failureRedirect: '/login' })(req, res, function () {
+				if(user.username === 'Admin'){
+					User.findOne({ username: user.username }).then((doc) => {
+						console.log("sucessfully logged the Admin in")
+						console.log(doc);
+						res.json({'message' : 'adminAcess',
+						'username': user.username
+					});
+					})	
+				}else{
+				User.findOne({ username: user.username }).then((doc) => {
 					console.log("sucessfully logged in")
 					console.log(doc);
-					res.json('sucess');
-          
-          
-				})
+					res.json({'message' : 'sucess',
+					'username': user.username
+				});
+				})	
+				}
 				
-		})
-			}
+			})
+		}
 	})
-
 })
 
 app.post("/api/register", (req,res)=>{
@@ -85,10 +100,30 @@ User.register(newUser, req.body.password , function(err){
     console.log("console: no errors in registering");
     passport.authenticate("local")(req, res, function(){     
         console.log("registered and saved in Mongodb");
-        res.json('sucess');
+        res.json({'message' : 'sucess',
+				  'username': newUser.username
+				 });
     });
   }
 });
+})
+
+//RESTfull APi for book data manupilation
+
+app.post("/books", function(req,res){
+
+	const newbook = new book ({
+	      name: req.body.name,
+	      author: req.body.author,
+	      price: req.body.price,
+	      quantity: req.body.quantity,
+	  })
+
+	console.log(newbook)
+	  
+	newbook.save();
+
+	  
 })
 
 app.listen(port, () => {
